d11e658cc00e4d2037705c64fde8eaa9504e3aa1
#manosenelcódigo <peligrosamente@gmail.com>
2016-05-24 23:20:01 -0300

    persona.php

---
+++ persona.php
@@ -0,0 +1,11 @@
+<?php
+require_once("implements/interface1.php");
+//abstract class Persona
+class Persona implements Interface1
+{
+    public function getNombre()
+    {
+        
+    }
+}
+$p=new Persona();
